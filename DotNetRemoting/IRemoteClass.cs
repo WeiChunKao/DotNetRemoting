acf9f02fad2b28a42dfcbbf0a23f8336a2028aa8
WeiChunKao <34979078+WeiChunKao@users.noreply.github.com>
2022-04-13 10:59:53 +0800

    Add files via upload

---
+++ DotNetRemoting/IRemoteClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoteClass
+{
+    public interface IRemoteClass:IDisposable
+    {
+        int Add(int a, int b);
+        int Multy(int a, int b);
+        int Divide(int a, int b);
+        int Substract(int a, int b);
+    }
+}
---
+++ DotNetRemoting/RemoteClass.cs
@@ -0,0 +1,35 @@
+﻿using RemoteClass;
+using System;
+
+namespace DotNetRemoting
+{
+    public class RemoteClass : MarshalByRefObject, IRemoteClass
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+                throw new Exception("Can't divid Zero");
+            return a / b;
+        }
+
+        public int Multy(int a, int b)
+        {
+            return a * b; 
+        }
+
+        public int Substract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
